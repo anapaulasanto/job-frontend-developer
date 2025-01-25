10bc4bcd0db745d2e12daa965d7d52a880a1f222
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2025-01-25 13:56:30 -0300

    fix news's card size img

--- test-news/src/components/NewsCard.jsx
+++ test-news/src/components/NewsCard.jsx
@@ -18,8 +18,8 @@ export default function NewsCard({ recentNews }) {
         <div className='flex flex-col gap-10 justify-center items-center '>
             {recentNews.map((item) => (
                 <div className='flex self-start items-center gap-4 cursor-pointer' key={item.title} onClick={() => newDetails(item)}>
-                    <div className='min-w-80'>
-                        <img className='rounded-md h-60 object-cover mx-auto' src={item.urlToImage} alt={item.title} />
+                    <div className='w-2/5'>
+                        <img className='rounded-md h-60 w-full object-cover mx-auto' src={item.urlToImage} alt={item.title} />
                     </div>
                     <div className='flex flex-col gap-2 self-start w-full'>
                         <p className='uppercase source-color font-bold text-xs font-roboto-condensed'>{item.source.name}</p>
