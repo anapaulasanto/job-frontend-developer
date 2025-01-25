7bee23976195687c5c9b121611b52766e8b62da1
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2025-01-25 03:32:01 -0300

    created function for formate date

--- test-news/src/components/NewsCard.jsx
+++ test-news/src/components/NewsCard.jsx
@@ -1,4 +1,5 @@
 import img from '../assets/Author.png'
+import { formatDate } from '../utils/shared/Date'
 
 export default function NewsCard({ recentNews }) {
     return (
@@ -9,13 +10,13 @@ export default function NewsCard({ recentNews }) {
                         <img className='rounded-md h-60 object-cover mx-auto' src={item.urlToImage} alt={item.title} />
                     </div>
                     <div className='flex flex-col gap-1 self-start w-full'>
-                        <p className='uppercase text-teal-600 font-bold text-xs font-roboto-condensed'>{item.source.name}</p>
-                        <h1 className='w-2/3 text-2xl font-bold hover:text-gray-800'>{item.title}</h1>
-                        <p className='w-3/5 font-roboto-condensed text-gray-500 text-sm'>{item.description}</p>
+                        <p className='uppercase source-color font-bold text-xs font-roboto-condensed'>{item.source.name}</p>
+                        <h1 className='w-2/3 title-color font-playfair text-2xl font-bold hover:text-gray-800'>{item.title}</h1>
+                        <p className='w-3/5 font-roboto-condensed content-color text-sm'>{item.description}</p>
                         <div className='flex flex-grow gap-2 items-center w-2/3'>
                             <img className='w-8 rounded-full' src={img} alt="" />
-                            <h3 className='text-xs font-bold font-poppins  text-slate-600'>{item.author}</h3>
-                            <p className='text-xs text-slate-600 font-poppins'>(item.publishedAt)</p>
+                            <h3 className='text-xs font-bold font-poppins  content-color'>{item.author}</h3>
+                            <p className='text-xs content-color font-poppins'>{formatDate(item.publishedAt)}</p>
                         </div>
                     </div>
                 </div>
