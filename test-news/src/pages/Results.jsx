6a4159d89913481aedd6353da19d28c87c193eef
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2025-01-25 09:37:33 -0300

    created page /results for results by  searchs in input

--- test-news/src/pages/Results.jsx
+++ test-news/src/pages/Results.jsx
@@ -1,7 +1,27 @@
+import { useContext } from "react";
+import { Context } from '../context/Context'
+import NavResults from "../components/templates/NavResults";
+import Input from "../components/Input";
+import NewsList from "../components/NewsList";
+
 export default function Results() {
-      return (
+    const { news, hasSearchResults } = useContext(Context);
+
+    return (
         <>
-            <div>results</div>
+            <header className='flex flex-col justify-center pb-11 w-full gap-10'>
+                <div >
+                    <NavResults />
+                </div>
+                <div className='w-full '>
+                    <Input />
+                </div>
+            </header>
+            <section className="flex items-center flex-col gap-5 p-5">
+                {hasSearchResults && (
+                    <NewsList news={news} texto="O resultado da sua busca" />
+                )}
+            </section>
         </>
     );
 }
